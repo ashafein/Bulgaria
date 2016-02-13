60821dedb9c7c226e06e220bf49cec6ad08cb1bf
Julianna <juliannaweb@gmail.com>
2016-02-13 11:00:32 +0200

    home page just a little and we done

--- js/all.js
+++ js/all.js
@@ -1,3 +1,11 @@
 /**
  * Created by vredian on 11.02.2016.
  */
+
+$('.realty-head').popover({
+    trigger: 'click',
+    animation: 'show',
+    placement: 'top',
+    html: '<div class="popover" role="tooltip"><div class="arrow"></div><div class="popover-content"></div></div>'
+
+});
