e2fdfa082f8faf22410fb6531edb0487552f3643
Julianna <juliannaweb@gmail.com>
2016-02-14 23:52:11 +0200

    product-page-done

--- js/all.js
+++ js/all.js
@@ -9,3 +9,25 @@ $('.realty-head').popover({
     html: '<div class="popover" role="tooltip"><div class="arrow"></div><div class="popover-content"></div></div>'
 
 });
+
+
+/* Including lightslider */
+
+$(document).ready(function() {
+    $('#productGallery').lightSlider({
+        gallery:true,
+        item:1,
+        loop:true,
+        thumbItem:4,
+        slideMargin:10,
+        controls: false,
+        enableDrag: false,
+        currentPagerPosition:'center',
+        onSliderLoad: function(el) {
+            el.lightGallery({
+                selector: '#productGallery .lslide'
+            });
+        }
+    });
+
+});
